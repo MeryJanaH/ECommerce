6109c8b2cad73c02fb86432c5d2db655cf26c958
Meryem_ANNOUAR <65432555+MeryJanaH@users.noreply.github.com>
2020-12-03 10:30:25 +0100

    about_us_fin

--- ECommerce/resources/views/website/backend/layouts/about-us.blade.php
+++ ECommerce/resources/views/website/backend/layouts/about-us.blade.php
@@ -14,34 +14,24 @@
 	<!--header-->
     @include('website.backend.layouts.header')
 
-	<!--main area-->
+    <!--main area-->
+</br>
 	<main id="main" class="main-site">
-
-		<div class="container">
-
-			<div class="wrap-breadcrumb">
-				<ul>
-					<li class="item-link"><a href="#" class="link">home</a></li>
-					<li class="item-link"><span>Contact us</span></li>
-				</ul>
-			</div>
-		</div>
-
 		<div class="container">
 			<!-- <div class="main-content-area"> -->
 				<div class="aboutus-info style-center">
 					<b class="box-title">Nos Occupations </b>
 					<p class="txt-content">
-nous sommes une équipe très passionnée par la ventre de plusieurs catégories de produits, donc nous sommes là pour vous faciliter la vie en mettant à votre disposition tout sorte de produits</p>
+Nous sommes une équipe très passionnée par la ventre de plusieurs catégories de produits, donc nous sommes là pour vous faciliter la vie en mettant à votre disposition tout sorte de produits</p>
 				</div>
 
 				<div class="row equal-container">
 					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 						<div class="aboutus-box-score equal-elem ">
-							<b class="box-score-title">10000</b>
+							<b class="box-score-title">Nos produits</b>
 							<span class="sub-title">produits disponible </span>
 							<p class="desc">
-des produits de très bonne qualité, On vous propose des centaines de millions de produits dans plus de 40 grandes catégories différentes, y compris l'électronique grand public, les machines et les vêtements.</div>
+Des produits de très bonne qualité, On vous propose des centaines de millions de produits dans plus de 40 grandes catégories différentes, y compris l'électronique grand public, les machines et les vêtements.</div>
 					</div>
 					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 						<div class="aboutus-box-score equal-elem ">
@@ -90,31 +80,23 @@ Lancé en 2005, AMWA est la principale plate-forme pour le commerce de gros mond
 
 					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 						<div class="aboutus-info style-small-left">
-							<b class="box-title">Cooperate with Us!</b>
+							<b class="box-title">Service</b>
 							<div class="list-showups">
 								<label>
-									<input type="radio" class="hidden" name="showup" id="shoup1" value="shoup1" checked="checked">
 									<span class="check-box"></span>
-									<span class='function-name'>Support 24/7</span>
-									<span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
+									<span class='function-name'>Support 7/24</span>
 								</label>
 								<label>
-									<input type="radio" class="hidden" name="showup" id="shoup2" value="shoup2">
 									<span class="check-box"></span>
-									<span class='function-name'>Best Quanlity</span>
-									<span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
+									<span class='function-name'>Excellent qualité</span>
 								</label>
 								<label>
-									<input type="radio" class="hidden" name="showup" id="shoup3" value="shoup3">
 									<span class="check-box"></span>
-									<span class='function-name'>Fastest Delivery</span>
-									<span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
+									<span class='function-name'>Livraison vite</span>
 								</label>
 								<label>
-									<input type="radio" class="hidden" name="showup" id="shoup4" value="shoup4">
 									<span class="check-box"></span>
-									<span class='function-name'>Customer Care</span>
-									<span class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</span>
+									<span class='function-name'>Service client</span>
 								</label>
 							</div>
 						</div>
@@ -133,79 +115,40 @@ Lancé en 2005, AMWA est la principale plate-forme pour le commerce de gros mond
 							data-margin="30"
 							data-responsive='{"0":{"items":"1"},"480":{"items":"2"},"768":{"items":"3"},"992":{"items":"3"},"1200":{"items":"4"}}' >
 
-							<div class="team-member equal-elem">
+                            <div class="team-member equal-elem">
 								<div class="media">
-									<a title="Meryem ANNOUAR">
-									<figure><img src="{{Storage::disk('local')->url('/mery_pic.jpeg')}}" alt="Meryem ANNOUAR"></figure>
-									</a>
-								</div>
-							</div>
-
-							<div class="team-member equal-elem">
-								<div class="media">
-									<a href="#" title="Wissal aarab">
-									<figure><img src="{{Storage::disk('local')->url('/wissal_pic.jpeg')}}" alt="Wissal Aarab"></figure>
-									</a>
-								</div>
-								<div class="info">
-									<b class="name">WISSAL AARAB</b>
-									<span class="title">Manager</span>
-									<p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text...</p>
-								</div>
-							</div>
-
-							<div class="team-member equal-elem">
-								<div class="media">
-									<a href="#" title="younes khamlichi">
+									<a href="#" title="Younes Khamlichi">
 									<figure><img src="{{Storage::disk('local')->url('/youness_pic.jpeg')}}"width="270px",height="300px", alt="youness khamlichi"></figure>
 									</a>
 								</div>
 								<div class="info">
 									<b class="name">YOUNES KHAMLICHI</b>
-									<span class="title">Marketer</span>
-									<p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text...</p>
+									<span class="title">Encadrant</span>
 								</div>
-							</div>
-
-							<div class="team-member equal-elem">
+                            </div>
+                        <pre></pre>
+                            <div class="team-member equal-elem">
 								<div class="media">
-									<a href="#" title="layla malak">
-									<figure><img src="assets/images/author-avata.jpg" alt="LAYLA malak"></figure>
-									</a>
+									<a href="#" title="Wissal Aarab">
+									<figure><img src="{{Storage::disk('local')->url('/wissal_pic.jpeg')}}" alt="Wissal Aarab"></figure>
 									</a>
 								</div>
 								<div class="info">
-									<b class="name">LAYLA MALAK</b>
-									<span class="title">Member</span>
-									<p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text...</p>
+									<b class="name">WISSAL AARAB</b>
+									<span class="title">Créateur</span>
 								</div>
-							</div>
-
-							<div class="team-member equal-elem">
-								<div class="media">
-									<a href="#" title="mohammed amin">
-										<figure><img src="assets/images/member-lucia.jpg" alt="mohammed amin"></figure>
+                            </div>
+                                                        <div class="team-member equal-elem">
+					 			<div class="media">
+									<a title="Meryem Annouar">
+									<figure><img src="{{Storage::disk('local')->url('/mery_pic.jpeg')}}" alt="Meryem ANNOUAR"></figure>
 									</a>
 								</div>
 								<div class="info">
-									<b class="name">mohammed amin</b>
-									<span class="title">Comptable</span>
-									<p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text...</p>
+									<b class="name">Meryem Annouar</b>
+									<span class="title">Créateur</span>
 								</div>
-							</div>
-
-							<div class="team-member equal-elem">
-								<div class="media">
-									<a href="#" title="NDIA maria">
-									<figure><img src="assets/images/member-nana.jpg" alt="NDIA maria"></figure>
-									</a>
-								</div>
-								<div class="info">
-									<b class="name">NANA maria</b>
-									<span class="title">ressource humain</span>
-									<p class="desc">Contrary to popular belief, Lorem Ipsum is not simply random text...</p>
-								</div>
-							</div>
+                            </div>
 
 						</div>
 

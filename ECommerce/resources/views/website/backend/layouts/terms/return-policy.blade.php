ab12287637a50b1f0f7aa8e620ab32976fe66602
Wissal-mery <wissal.wiiwii.17@gmail.com>
2020-12-04 21:57:15 +0100

    dernier modif

--- ECommerce/resources/views/website/backend/layouts/terms/return-policy.blade.php
+++ ECommerce/resources/views/website/backend/layouts/terms/return-policy.blade.php
@@ -30,18 +30,43 @@
 		<div class="container pb-60">
 			<div class="row">
 				<div class="col-md-12">
-					<h3>Return Policy</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-				</div>
+					<h1>POLITIQUE DE RETOURS</h1>
+					<p>Merci pour votre achat. Nous espérons que vous êtes satisfait de votre achat. Cependant, si vous n'êtes pas entièrement satisfait de votre achat pour 
+quelque raison que ce soit, vous pouvez nous le retourner pourcrédit magasin ou échange. Veuillez voir ci-dessous pour plus d'informations sur notre 
+politique de retour. </p>
+<h2>RETOUR</h2>
+<p>Tous les retours doivent être oblitérés dans sept (7)jours de la date d'achat. Tous les articles retournés doivent être dans un état neuf et inutilisé, avec 
+toutes les étiquettes et étiquettes d'origine attachées. </p>
+<h2>PROCESSUS DE RETOUR </h2>
+<p>Pour retourner un article, veuillez envoyer un e-mail au service client à wissal.wiiwii.17@gmail.com
+pour obtenir un numéro d'autorisation de retour de marchandise 
+(RMA). Après avoir reçu un numéro RMA, placez l'article en toute sécurité dans son emballage d'origine et le formulaire de retour fourni, et postez votre 
+</p>
+<p>retour à l'adresse suivante: </p>
+<ul>
+	<li>AMVVA </li>
+<li>Attn: retours </li>
+<li>RMA # </li>
+<li>fes rue industrielle </li>
+<li>fes, fes rue industrielle num 30 2005 </li>
+<li>Maroc </li>
+</ul>
+<p>Veuillez noter que vous serez responsable de tous les frais d'expédition de retour. Nous vous recommandons tortement d'utiliser une méthode traçable 
+pour poster votre retour. 
+</p>
+<h2>REMBOURSEMENTS </h2>
+<p>Après avoir reçu votre retour et inspecté l'état de votre article, nous traiterons votre retour ou échange. Veuillez autoriser au moinstrois (3) jours à 
+compter de la réception de votre article pour traiter votre retour ou échange. Nous vous informerons par e-mail lorsque votre retour aura été traité. 
+</p>
+<h2>DES EXCEPTIONS </h2>
+<p>Pour les produits défectueux ou endommagés, veuillez nous contacter au numéro de service client ci-dessous pour organiser un remboursement ou un 
+échange. </p>
+<h2>QUESTIONS</h2>
+<p>si vous avez des questions concernant notre politique de retour, veuillez contacter à : wissal.wiiwii.17@gmail.com</p>
+
+
+
+</div>
 			</div>
 		</div><!--end container-->
 
--- ECommerce/resources/views/website/backend/layouts/terms/terms-conditions.blade.php
+++ ECommerce/resources/views/website/backend/layouts/terms/terms-conditions.blade.php
@@ -30,18 +30,115 @@
 		<div class="container pb-60">
 			<div class="row">
 				<div class="col-md-12">
-					<h3>Terms & Conditions</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-				</div>
+				<h2><strong>Termes et Conditions</strong></h2>
+
+<p>Bienvenue à AMWA!</p>
+
+<p>Ces termes et conditions décrivent les règles et réglementations relatives à l'utilisation du site Web d'AMWA, situé à www.AMWA.com.</p>
+
+<p> En accédant à ce site Web, nous supposons que vous acceptez ces termes et conditions. Ne continuez pas à utiliser AMWA si vous n'acceptez pas toutes les conditions générales énoncées sur cette page.</p>
+
+<p>La terminologie suivante s'applique aux présentes conditions générales, à la déclaration de confidentialité et à l'avis de non-responsabilité et à tous les accords: «Client», «Vous» et «Votre» se réfèrent à vous, la personne connectée à ce site Web et conforme aux termes et conditions de la société. «L'entreprise», «nous-mêmes», «nous», «nos» et «nous» désignent notre entreprise. «Partie», «Parties» ou «Nous» désigne à la fois le Client et nous-mêmes. Toutes les conditions font référence à l'offre, à l'acceptation et à la prise en compte du paiement nécessaires pour entreprendre le processus de notre assistance au client de la manière la plus appropriée dans le but exprès de répondre aux besoins du client en ce qui concerne la fourniture des services déclarés de la société, conformément à et sous réserve de la loi en vigueur aux Pays-Bas.</p>
+
+<h3><strong>Cookies</strong></h3>
+
+
+
+<p>Nous utilisons des cookies. En accédant à AMWA, vous avez accepté d'utiliser des cookies en accord avec la politique de confidentialité de l'AMWA.</p>
+
+<h3><strong>License</strong></h3>
+
+<p>Sauf indication contraire, AMWA et / ou ses concédants de licence détiennent les droits de propriété intellectuelle pour tout le matériel sur AMWA. Tous les droits de propriété intellectuelle sont réservés. Vous pouvez y accéder depuis AMWA pour votre usage personnel, sous réserve des restrictions définies dans ces termes et conditions.</p>
+
+<p>Vous ne devez pas:</p>
+<ul>
+    <li>Republier le matériel d'AMWA</li>
+    <li>Vendre, louer ou sous-licencier du matériel d'AMWA</li>
+    <li>Reproduire, dupliquer ou copier du matériel d'AMWA</li>
+    <li>Redistribuer le contenu d'AMWA</li>
+</ul>
+
+
+
+<p>Certaines parties de ce site Web offrent aux utilisateurs la possibilité de publier et d'échanger des opinions et des informations dans certaines zones du site Web. AMWA ne filtre pas, ne modifie pas, ne publie pas et ne révise pas les commentaires avant leur présence sur le site Web. Les commentaires ne reflètent pas les vues et opinions d'AMWA, de ses agents et / ou affiliés. Les commentaires reflètent les points de vue et les opinions de la personne qui publie ses points de vue et opinions. Dans la mesure permise par les lois applicables, AMWA ne sera pas responsable des commentaires ou de toute responsabilité, dommages ou dépenses causés et / ou subis à la suite de toute utilisation et / ou publication et / ou apparence des commentaires sur ce site Internet.</p>
+
+<p>AMWA se réserve le droit de surveiller tous les commentaires et de supprimer tout commentaire qui peut être considéré comme inapproprié, offensant ou qui entraîne une violation des présentes conditions générales.</p>
+
+<p>Vous garantissez et déclarez que:</p>
+
+<ul>
+    <li>Vous avez le droit de publier les commentaires sur notre site Web et disposez de toutes les licences et consentements nécessaires pour le faire;</li>
+    <li>Les Commentaires n'envahissent aucun droit de propriété intellectuelle, y compris, sans limitation, les droits d'auteur, brevets ou marques de commerce d'un tiers;</li>
+    <li>Les commentaires ne contiennent aucun élément diffamatoire, calomnieux, offensant, indécent ou autrement illégal qui constitue une atteinte à la vie privée</li>
+    <li>Les commentaires ne seront pas utilisés pour solliciter ou promouvoir des activités commerciales ou personnalisées ou présenter des activités commerciales ou des activités illégales.</li>
+</ul>
+
+<p>Vous accordez par la présente à AMWA une licence non exclusive pour utiliser, reproduire, éditer et autoriser des tiers à utiliser, reproduire et éditer n'importe lequel de vos commentaires sous toutes formes, formats ou supports.</p>
+
+<h3><strong>Hyperliens vers notre contenu</strong></h3>
+
+<p>Les organisations suivantes peuvent créer un lien vers notre site Web sans autorisation écrite préalable:</p>
+
+<ul>
+    <li>Organismes gouvernementaux;</li>
+    <li>Moteurs de recherche;</li>
+    <li>Organisations de presse;</li>
+    <li>Les distributeurs d'annuaires en ligne peuvent créer un lien vers notre site Web de la même manière qu'ils créent des liens hypertexte vers les sites Web d'autres entreprises cotées; </li>
+    <li>Entreprises accréditées à l'échelle du système, à l'exception de la sollicitation d'organisations à but non lucratif, de centres commerciaux caritatifs et de groupes de collecte de fonds caritatifs qui ne peuvent pas créer de lien hypertexte vers notre site Web.</li>
+</ul>
+
+<p>Ces organisations peuvent créer un lien vers notre page d'accueil, vers des publications ou vers d'autres informations sur le site Web à condition que le lien: (a) ne soit en aucun cas trompeur; (b) n'implique pas à tort un parrainage, une approbation ou une approbation de la partie liante et de ses produits et / ou services; et (c) s'inscrit dans le contexte du site de la partie liante.</p>
+
+<p>Nous pouvons considérer et approuver d'autres demandes de lien provenant des types d'organisations suivants:</p>
+
+<ul>
+    <li>des sources d'informations bien connues sur les consommateurs et / ou les entreprises;</li>
+    <li>sites communautaires dot.com;</li>
+    <li>associations ou autres groupes représentant des organismes de bienfaisance;</li>
+    <li>distributeurs d'annuaires en ligne;</li>
+    <li>portails Internet;</li>
+    
+</ul>
+
+
+
+<h3><strong>iFrames</strong></h3>
+
+<p>Sans approbation préalable et autorisation écrite, vous ne pouvez pas créer de cadres autour de nos pages Web qui modifient de quelque manière que ce soit la présentation visuelle ou l'apparence de notre site Web.</p>
+
+<h3><strong>Responsabilité du contenu</strong></h3>
+
+<p>Nous ne serons pas tenus responsables de tout contenu qui apparaît sur votre site Web. Vous acceptez de nous protéger et de nous défendre contre toutes les réclamations qui s'élèvent sur votre site Web. Aucun lien ne doit apparaître sur un site Web qui peut être interprété comme diffamatoire, obscène ou criminel, ou qui enfreint, viole ou préconise la violation ou toute autre violation des droits de tiers.</p>
+
+<h3><strong>Votre vie privée</strong></h3>
+
+<p>Veuillez lire la politique de confidentialité</p>
+
+<h3><strong>Réservation de droits</strong></h3>
+
+<p>Nous nous réservons le droit de vous demander de supprimer tous les liens ou tout lien particulier vers notre site Web. Vous acceptez de supprimer immédiatement tous les liens vers notre site Web sur demande. Nous nous réservons également le droit de modifier ces termes et conditions et sa politique de liaison à tout moment. En vous connectant en permanence à notre site Web, vous acceptez d'être lié et de suivre ces termes et conditions de liaison.</p>
+
+<h3><strong>Suppression des liens de notre site Web</strong></h3>
+
+<p>Si vous trouvez un lien sur notre site Web qui est offensant pour une raison quelconque, vous êtes libre de nous contacter et de nous informer à tout moment. Nous examinerons les demandes de suppression de liens, mais nous ne sommes pas obligés de le faire ou de vous répondre directement.</p>
+
+<p>Nous ne garantissons pas que les informations sur ce site Web sont correctes, nous ne garantissons pas leur exhaustivité ou leur exactitude; nous ne promettons pas non plus de garantir que le site Web reste disponible ou que le matériel sur le site Web est tenu à jour.</p>
+
+<h3><strong>Avertissement</strong></h3>
+
+<p>Dans la mesure maximale permise par la loi applicable, nous excluons toutes les représentations, garanties et conditions relatives à notre site Web et à l'utilisation de ce site Web. Rien dans cette clause de non-responsabilité:</p>
+
+<ul>
+    <li>limiter ou exclure notre ou votre responsabilité en cas de décès ou de blessures corporelles;</li>
+    <li>limiter ou exclure notre ou votre responsabilité en cas de fraude ou de fausse déclaration frauduleuse;</li>
+    <li>limiter l'une de nos ou vos responsabilités d'une manière non autorisée par la loi applicable;</li>
+    <li>exclure l'une de nos ou vos responsabilités qui ne peuvent être exclues en vertu de la loi applicable.</li>
+</ul>
+
+<p>Les limitations et interdictions de responsabilité définies dans la présente section et ailleurs dans cette clause de non-responsabilité: (a) sont soumises au paragraphe précédent; et (b) régir toutes les responsabilités découlant de la clause de non-responsabilité, y compris les responsabilités découlant d'un contrat, d'un délit et d'un manquement à une obligation légale.</p>
+
+<p>Tant que le site Web et les informations et services sur le site Web sont fournis gratuitement, nous ne serons pas responsables de toute perte ou dommage de quelque nature que ce soit.</p>
+</div>
 			</div>
 		</div><!--end container-->
 

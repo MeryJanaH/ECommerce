5d0c3d99b3fbe1e9946965568eaa01c2d11ef34f
Wissal-mery <wissal.wiiwii.17@gmail.com>
2020-12-04 18:06:06 +0100

    privacy

--- ECommerce/resources/views/website/backend/layouts/privacy-policy.blade.php
+++ ECommerce/resources/views/website/backend/layouts/privacy-policy.blade.php
@@ -30,17 +30,121 @@
 		<div class="container pb-60">
 			<div class="row">
 				<div class="col-md-12">
-					<h3>Privacy Policy</h3>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor voluptates explicabo atque. Aperiam, esse nemo reprehenderit nisi excepturi repudiandae vero aspernatur dicta sed blanditiis. Sequi quidem cum maiores sed architecto.</p>
+					<h2>politique de confidentialité</h2>
+					<p>Cette politique de confidentialité décrit nos politiques et procédures sur la collecte, l'utilisation et la divulgation de vos informations lorsque vous utilisez le service et vous informe de vos droits à la confidentialité et de la manière dont la loi vous protège.</p>
+
+<p>Nous utilisons vos données personnelles pour fournir et améliorer le service. En utilisant le Service, vous acceptez la collecte et l'utilisation d'informations conformément à la présente politique de confidentialité. Cette politique de confidentialité a été créée avec l'aide du générateur de politique de confidentialité .</p>
+<h3>Interprétation et définitions</h3>
+<p>Les mots dont la lettre initiale est en majuscule ont des significations définies dans les conditions suivantes. Les définitions suivantes ont la même signification, qu'elles apparaissent au singulier ou au pluriel.
+</p>
+<h3>Définitions</h3>
+<p> <h4>Aux fins de cette politique de confidentialité:</h4>
+<ul>
+
+<li>
+<p><p><b>Compte </b>signifie un compte unique créé pour que vous puissiez accéder à notre service ou à des parties de notre service.</p></li>
+
+<li><p><b>La société</b> (appelée «la société», «nous», «notre» ou «notre» dans le présent accord) fait référence à AMWA.</p></li>
+
+<li><p><b>Les cookies</b> sont de petits fichiers placés sur votre ordinateur, appareil mobile ou tout autre appareil par un site Web, contenant les détails de votre historique de navigation sur ce site Web parmi ses nombreuses utilisations.</p></li>
+
+<li><p><b>Pays</b> se réfère à: Maroc</li>
+
+<li><p><b>Appareil </b>désigne tout appareil pouvant accéder au Service tel qu'un ordinateur, un téléphone portable ou une tablette numérique.</p></li>
+
+<li><p><b>Les données</b> personnelles sont toute information relative à une personne identifiée ou identifiable.</p></li>
+
+<li><p><b>Le service</b> fait référence au site Web.</li>
+
+<li><p><b>Prestataire </b>de services désigne toute personne physique ou morale qui traite les données pour le compte de la Société. Il fait référence à des sociétés tierces ou à des personnes employées par la Société pour faciliter le Service, pour fournir le Service au nom de la Société, pour exécuter des services liés au Service ou pour aider la Société à analyser la façon dont le Service est utilisé.<p></li>
+
+<li><p><b>Le service</b> de médias sociaux tiers fait référence à tout site Web ou à tout site Web de réseau social via lequel un utilisateur peut se connecter ou créer un compte pour utiliser le service.</p></li>
+
+<li><p><b>Les données</b> d'utilisation font référence aux données collectées automatiquement, soit générées par l'utilisation du service, soit à partir de l'infrastructure du service elle-même (par exemple, la durée d'une visite de page).<p></li>
+
+<li><p><b>Le site </b>Web fait référence à AMWA, accessible depuis http://AMWA.com</p></li>
+
+<li><p><b>Vous</b> désigne la personne accédant ou utilisant le Service, ou la société, ou toute autre entité juridique au nom de laquelle cette personne accède ou utilise le Service, selon le cas.</p></li>
+</ul>
+</p>
+<h1>Collecte et utilisation de vos données personnelles</h1>
+<h2>Types de données collectées</h2>
+<h3>Données personnelles</h3>
+<p>Lors de l'utilisation de notre service, nous pouvons vous demander de nous fournir certaines informations personnellement identifiables qui peuvent être utilisées pour vous contacter ou vous identifier. Les informations personnellement identifiables peuvent inclure, mais sans s'y limiter:
+</p>
+<ul>
+<li>
+<p>Adresse Email</p>
+</li>
+<li>
+<p>Prénom et nom</p>
+</li>
+<li>
+<p>Numéro de téléphone</p>
+</li>
+<li>
+<p>Données d'utilisation</p>
+</li>
+</ul>
+<h3> Données d'utilisation</h3>
+<p>Les données d'utilisation sont collectées automatiquement lors de l'utilisation du service.
+</p>
+<p>Les données d'utilisation peuvent inclure des informations telles que l'adresse de protocole Internet de votre appareil (par exemple, l'adresse IP), le type de navigateur, la version du navigateur, les pages de notre service que vous visitez, l'heure et la date de votre visite, le temps passé sur ces pages, l'appareil unique identifiants et autres données de diagnostic.</p>
+<p>Lorsque vous accédez au service par ou via un appareil mobile, nous pouvons collecter certaines informations automatiquement, y compris, mais sans s'y limiter, le type d'appareil mobile que vous utilisez, l'identifiant unique de votre appareil mobile, l'adresse IP de votre appareil mobile, votre mobile système d'exploitation, le type de navigateur Internet mobile que vous utilisez, les identifiants uniques de l'appareil et d'autres données de diagnostic.</p>
+<p>Nous pouvons également collecter des informations que votre navigateur envoie chaque fois que vous visitez notre service ou lorsque vous accédez au service par ou via un appareil mobile.</p>
+<h3>Technologies de suivi et cookies</h3>
+<p>Nous utilisons des cookies et des technologies de suivi similaires pour suivre l'activité sur notre service et stocker certaines informations. Les technologies de suivi utilisées sont des balises, des balises et des scripts pour collecter et suivre des informations et pour améliorer et analyser notre service. Les technologies que nous utilisons peuvent inclure:</p>
+<ul>
+<ul>
+<li><strong>Cookies ou cookies de navigateur</strong> Un cookie est un petit fichier placé sur votre appareil. Vous pouvez demander à votre navigateur de refuser tous les cookies ou d'indiquer quand un cookie est envoyé. Cependant, si vous n'acceptez pas les cookies, il se peut que vous ne puissiez pas utiliser certaines parties de notre service. À moins que vous n'ayez ajusté les paramètres de votre navigateur pour qu'il refuse les cookies, notre service peut utiliser des cookies.</li>
+<li><strong>Cookies Flash.</strong>Certaines fonctionnalités de notre service peuvent utiliser des objets stockés localement (ou des cookies Flash) pour collecter et stocker des informations sur vos préférences ou votre activité sur notre service. Les cookies Flash ne sont pas gérés par les mêmes paramètres de navigateur que ceux utilisés pour les cookies de navigateur. Pour plus d'informations sur la manière de supprimer les cookies Flash, veuillez lire "Où puis-je modifier les paramètres de désactivation ou de suppression des objets partagés locaux?" disponible à l' adresse https://helpx.adobe.com/flash-player/kb/disable-local-shared-objects-flash.html#main_Where_can_I_change_the_settings_for_disabling__or_deleting_local_shared_objects_</li>
+<li><strong>Balises Web.</strong>Certaines sections de notre service et de nos e-mails peuvent contenir de petits fichiers électroniques appelés balises Web (également appelés gifs clairs, pixels invisibles et gifs à pixel unique) qui permettent à l'entreprise, par exemple, de compter les utilisateurs qui ont visité ces pages. ou a ouvert un e-mail et pour d'autres statistiques de site Web connexes (par exemple, enregistrer la popularité d'une certaine section et vérifier l'intégrité du système et du serveur).</li></ul>
+<p>Les cookies peuvent être des cookies «persistants» ou «de session». Les cookies persistants restent sur votre ordinateur personnel ou appareil mobile lorsque vous vous déconnectez, tandis que les cookies de session sont supprimés dès que vous fermez votre navigateur Web. En savoir plus sur les cookies: que sont les cookies? .</p>
+<p>Nous utilisons à la fois des cookies de session et des cookies persistants aux fins décrites ci-dessous:</p>
+<ul>
+<li>
+<p><strong>Cookies nécessaires / essentiels</strong></p>
+<p>Type: Cookies de session</p>
+<p>Administré par: nous</p>
+<p>Objectif: Ces cookies sont essentiels pour vous fournir les services disponibles sur le site Web et pour vous permettre d'utiliser certaines de ses fonctionnalités. Ils aident à authentifier les utilisateurs et à empêcher l'utilisation frauduleuse des comptes d'utilisateurs. Sans ces cookies, les services que vous avez demandés ne peuvent pas être fournis et nous n'utilisons ces cookies que pour vous fournir ces services.</p>
+
+</li>
+<li>
+<p><strong>Politique de cookies / Avis d'acceptation des cookies</strong></p>
+<p>Type: Cookies persistants</p>
+<p>Administré par: nous</p>
+<p>Finalité: Ces cookies identifient si les utilisateurs ont accepté l'utilisation de cookies sur le site Web.</p>
+</li>
+<li>
+<p><strong>Cookies de fonctionnalité</strong></p>
+<p>Type: Cookies persistants</p>
+<p>Administré par: nous</p>
+<p>Objectif: ces cookies nous permettent de nous souvenir des choix que vous faites lorsque vous utilisez le site Web, tels que la mémorisation de vos informations de connexion ou de votre préférence de langue. Le but de ces cookies est de vous offrir une expérience plus personnelle et de vous éviter d'avoir à ressaisir vos préférences chaque fois que vous utilisez le site Web.</p>
+
+<h1>Informations détaillées sur le traitement de vos données personnelles</h1>
+<p>Les fournisseurs de services que nous utilisons peuvent avoir accès à vos données personnelles. Ces fournisseurs tiers collectent, stockent, utilisent, traitent et transfèrent des informations sur votre activité sur notre service conformément à leurs politiques de confidentialité.</p>
+<h1>Utilisation, performances et divers</h1>
+<p>Nous pouvons utiliser des fournisseurs de services tiers pour améliorer notre service.</p>
+<ul>
+<li>Google Adresses</li>
+</ul>
+<p>Google Places est un service qui renvoie des informations sur les lieux à l'aide de requêtes HTTP. Il est exploité par Google
+
+Le service Google Places peut collecter des informations auprès de vous et de votre appareil à des fins de sécurité.</p>
+<h1>Liens vers d'autres sites Web</h1>
+<p>Notre service peut contenir des liens vers d'autres sites Web qui ne sont pas exploités par nous. Si vous cliquez sur un lien tiers, vous serez dirigé vers le site de ce tiers. Nous vous conseillons vivement de consulter la politique de confidentialité de chaque site que vous visitez.</p>
+<p>Nous n'avons aucun contrôle et n'assumons aucune responsabilité pour le contenu, les politiques de confidentialité ou les pratiques de tout site ou service tiers.</p>
+<h1>Modifications de cette politique de confidentialité</h1>
+<p>Nous pouvons mettre à jour notre politique de confidentialité de temps à autre. Nous vous informerons de tout changement en publiant la nouvelle politique de confidentialité sur cette page.</p>
+<p>Nous vous informerons par e-mail et / ou par un avis visible sur notre service, avant que le changement ne devienne effectif, et nous mettrons à jour la date de «dernière mise à jour» en haut de cette politique de confidentialité.</p>
+<p>Il vous est conseillé de consulter périodiquement cette politique de confidentialité pour tout changement. Les modifications apportées à cette politique de confidentialité sont effectives lorsqu'elles sont publiées sur cette page.</p>
+<h1>Nous contacter</h1>
+<p>Si vous avez des questions sur cette politique de confidentialité, vous pouvez nous contacter:</p>
+<ul>
+<li>Par email: wissal.wiiwii.17@gmail.com
+</li>
+</ul>
+
 				</div>
 			</div>
 		</div><!--end container-->

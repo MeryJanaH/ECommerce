3b018cb5031fec49c969ef75884cde0d15cc56fa
Meryem_ANNOUAR <65432555+MeryJanaH@users.noreply.github.com>
2020-12-13 22:37:40 +0100

    pay

--- ECommerce/resources/views/website/backend/layouts/payment/paypage.blade.php
+++ ECommerce/resources/views/website/backend/layouts/payment/paypage.blade.php
@@ -30,22 +30,11 @@
 					<div class="summary-item payment-method">
 						<h4 class="title-box">Payment Method</h4>
 						<div class="choose-payment-methods">
-							<label class="payment-method">
-								<input name="payment-method" id="payment-method-bank" value="bank" type="radio">
-								<span>Direct Bank Transder</span>
-								<span class="payment-desc">But the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</span>
-							</label>
-							<label class="payment-method">
-								<input name="payment-method" id="payment-method-visa" value="visa" type="radio">
-								<span>visa</span>
-								<span class="payment-desc">There are many variations of passages of Lorem Ipsum available</span>
-							</label>
-							<label class="payment-method">
-								<input name="payment-method" id="payment-method-paypal" value="paypal" type="radio">
-								<span>Paypal</span>
-								<span class="payment-desc">You can pay with your credit</span>
-								<span class="payment-desc">card if you don't have a paypal account</span>
-							</label>
+							  <script
+                                    src="https://www.paypal.com/sdk/js?client-id=AfGKpUUXTx91JusgwTm0v099gUjXWcWhaGvQukjql8hZxjh4AcKGFW7Pn5Nmkn1VIOL6ZvFw_zRdVKiV"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+                              </script>
+
+                              <div id="paypal-button-container"></div>
                         </div>
                     </br></br></br>
                         <p class="summary-info grand-total"><span>Totale</span> <span class="grand-total-price">${{$pay_info['price']}}</span></p>
@@ -54,7 +43,6 @@
                         @else
                         <p class="summary-info grand-total"><span>Status</span> <span class="grand-total-price">Paiement accomplis</span></p>
                         @endif
-                        <a href="thankyou.html" class="btn btn-medium">Payer</a>
                 </div>
             </br></br></br>
 			</div><!--end main content area-->
@@ -70,5 +58,28 @@
 	<!--footer area-->
 
 </body>
-</html>
 
+<script>
+  paypal.Buttons({
+    createOrder: function(data, actions) {
+      // This function sets up the details of the transaction, including the amount and line item details.
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: {{$pay_info['price']}}
+          }
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      // This function captures the funds from the transaction.
+      return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
+        alert('Transaction completed by ' + details.payer.name.given_name);
+      });
+    }
+  }).render('#paypal-button-container');
+  //This function displays Smart Payment Buttons on your web page.
+</script>
+
+</html>
